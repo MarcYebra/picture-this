64912ba7f9556b8daf4c13ee416ca7b6da295557
Marc Yebra <106407943+MarcYebra@users.noreply.github.com>
2022-11-16 14:04:23 -0500

    no form yet

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,9 @@ Rails.application.routes.draw do
   root 'homes#index'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get '/photographers/new', to: 
+  # get '/photographers/new', to: 
+ 
   resources :photographers
-    re
+    
 
 end
