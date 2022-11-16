d49ca64eb10cb5073fce95e0680a59f6f4f7b072
Marc Yebra <106407943+MarcYebra@users.noreply.github.com>
2022-11-16 14:28:59 -0500

    updates with making the index page with React instead of Rails

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,12 @@ Rails.application.routes.draw do
   root 'homes#index'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get '/photographers/new', to: 
-  resources :photographers
-    re
+  get '/photographers', to: 'static_pages#index'
+  # get '/photographers/new', to: 
 
+  namespace :api do
+    namespace :v1 do
+      resources :photographers, only: [:index, :show, :create]
+    end
+  end 
 end
