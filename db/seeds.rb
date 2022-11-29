a86821f04f82b5170873216fc1fc23695e6a763c
Marc Yebra <106407943+MarcYebra@users.noreply.github.com>
2022-11-29 16:00:09 -0500

    seed updates

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,6 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+User.delete_all
+Photographer.delete_all
+Review.delete_all
 
 #### User ####
 user_1 = User.create(
