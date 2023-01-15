b93733c02fcada862ea83e70bab8fb51c04c063c
Marc Yebra <106407943+MarcYebra@users.noreply.github.com>
2023-01-15 17:51:54 -0600

    part one of getting image carousel to function

--- app/javascript/react/components/Photographer-Index/BannerImage.js
+++ app/javascript/react/components/Photographer-Index/BannerImage.js
@@ -4,7 +4,7 @@ const BannerImage = (props) => {
   return (
     <div className='photographer-index-sign-up-button'>
       <div className='photographer-index-image-banner'>
-        <img src='https://picture-this-production.s3.amazonaws.com/Home/Photographer-Index.png' alt='create account' />
+        <img src='https://picture-this-production.s3.amazonaws.com/Photographer-Index-Banner-Images/Photographer-Index-Photographers.png' alt='create account' />
       </div>
         <div className='photographer-index-join-container'>
           <div className='photographer-index-image-banner-title'>Join our <i>talented</i><br/>group of <i><br/>photographers</i></div>
