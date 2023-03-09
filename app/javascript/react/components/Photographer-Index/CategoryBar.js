02ee055974d75566d8a46c6fdd81281783b5d35f
Marc Yebra <106407943+MarcYebra@users.noreply.github.com>
2023-03-09 10:24:19 -0600

    updated button style

--- app/javascript/react/components/Photographer-Index/CategoryBar.js
+++ app/javascript/react/components/Photographer-Index/CategoryBar.js
@@ -6,19 +6,19 @@ const CategoryBar = (props) => {
   return (
     <div>
       <div className='categories-container'>
-        <Link to='/photographers'><div className='categories-button'>View All</div></Link>  
-        <Link to='/celebrity'><div className='categories-button'>Celebrity</div></Link>  
-        <Link to='/engagement'><div className='categories-button'>Engagement</div></Link>  
-        <Link to='/event'><div className='categories-button'>Event</div></Link>  
-        <Link to='/family'><div className='categories-button'>Family</div></Link>  
-        <Link to='/fashion'><div className='categories-button'>Fashion</div></Link>  
-        <Link to='/fitness'><div className='categories-button'>Fitness</div></Link>  
-        <Link to='/food'><div className='categories-button'>Food</div></Link>  
-        <Link to='/graduation'><div className='categories-button'>Graduation</div></Link>  
-        <Link to='/pet'><div className='categories-button'>Pet</div></Link>  
-        <Link to='/portrait'><div className='categories-button'>Portrait</div></Link>  
-        <Link to='/sport'><div className='categories-button'>Sport</div></Link>  
-        <Link to='/wedding'><div className='categories-button'>Wedding</div></Link>  
+        <Link to='/photographers'><div className='category-button'>View All</div></Link>  
+        <Link to='/celebrity'><div className='category-button'>Celebrity</div></Link>  
+        <Link to='/engagement'><div className='category-button'>Engagement</div></Link>  
+        <Link to='/event'><div className='category-button'>Event</div></Link>  
+        <Link to='/family'><div className='category-button'>Family</div></Link>  
+        <Link to='/fashion'><div className='category-button'>Fashion</div></Link>  
+        <Link to='/fitness'><div className='category-button'>Fitness</div></Link>  
+        <Link to='/food'><div className='category-button'>Food</div></Link>  
+        <Link to='/graduation'><div className='category-button'>Graduation</div></Link>  
+        <Link to='/pet'><div className='category-button'>Pet</div></Link>  
+        <Link to='/portrait'><div className='category-button'>Portrait</div></Link>  
+        <Link to='/sport'><div className='category-button'>Sport</div></Link>  
+        <Link to='/wedding'><div className='category-button'>Wedding</div></Link>  
       </div>
     </div>
   )
